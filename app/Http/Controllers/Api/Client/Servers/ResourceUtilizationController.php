ed2b1e1293db5a109b7806a9d617c9de4a76e40e
ybeta <syntemcpe@gmail.com>
2025-12-27 03:06:46 +0100

    Remove caching server resource utilization

--- app/Http/Controllers/Api/Client/Servers/ResourceUtilizationController.php
+++ app/Http/Controllers/Api/Client/Servers/ResourceUtilizationController.php
@@ -26,9 +26,8 @@ class ResourceUtilizationController extends ClientApiController
     /**
      * View resources
      *
-     * Return the current resource utilization for a server. This value is cached for up to
-     * 20 seconds at a time to ensure that repeated requests to this endpoint do not cause
-     * a flood of unnecessary API calls.
+     * Return the current resource utilization for a server. This value is no longer cached
+     * and provides real-time data.
      *
      * @return array<array-key, mixed>
      *
@@ -36,10 +35,7 @@ class ResourceUtilizationController extends ClientApiController
      */
     public function __invoke(GetServerRequest $request, Server $server): array
     {
-        $key = "resources:$server->uuid";
-        $stats = $this->cache->remember($key, Carbon::now()->addSeconds(20), function () use ($server) {
-            return $this->repository->setServer($server)->getDetails();
-        });
+        $stats = $this->repository->setServer($server)->getDetails();
 
         return $this->fractal->item($stats)
             ->transformWith($this->getTransformer(StatsTransformer::class))
